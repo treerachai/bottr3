d94f08de2ab4aa49881187c2c01a6d1546e8cf28
™Ŧяәәƅoŧ <nutech@outlook.com>
2018-10-11 05:38:21 +0700

    Add files via upload

--- line/linepy/config.py
+++ line/linepy/config.py
@@ -34,7 +34,7 @@ class Config(object):
     APP_TYPE    = ApplicationType._VALUES_TO_NAMES[304]
     APP_VER     = '8.12.2'
     CARRIER     = '51089, 1-0'
-    SYSTEM_NAME = 'TRBOT'
+    SYSTEM_NAME = 'HelloWorld'
     SYSTEM_VER  = '11.2.5'
     IP_ADDR     = '8.8.8.8'
     EMAIL_REGEX = re.compile(r"[^@]+@[^@]+\.[^@]+")
